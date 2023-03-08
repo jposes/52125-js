dd20d50ac875c6c181acf8caec3b740831e601c7
Daniel Riverol <dannyriverol@hotmail.com>
2023-03-08 12:11:05 -0300

    Comisión - 52125

---
+++ clase_1/js/main.js
@@ -0,0 +1,52 @@
+//variables
+/* var nombre = "Dani";//ES5
+var nombre = "Diego"; */
+
+//declaracion
+let apellido = "Gomez"; //ES6
+let apellido1 = "Perez Gonzalez";
+
+let nombre;
+let nombreCompleto;
+let numero1 = 33;
+//asignacion
+let numero;
+numero = 12;
+
+//reasignacion de valores
+apellido1 = "Perez Perez";
+
+/* apellido1 = 23; */
+const PI = 3.14159;
+const IVA = 21;
+nombre = "Dani";
+nombreCompleto = nombre + " " + apellido;//concatenacion
+
+let resultado = numero * numero1;
+
+let fraseNumero = numero + apellido1;//concatenando
+
+//metodos de salida
+/* console.log(nombre);
+console.log(PI);
+console.log("El nombre completo es: " + nombreCompleto); */
+console.log(numero);
+console.log(numero1);
+console.log(resultado);
+
+
+console.log(fraseNumero);
+
+/* alert("Hola " + nombre) */
+
+//metodo de entrada
+/* let nombre2 = prompt("Ingresa tu nombre");
+
+console.log(nombre2); */
+
+let n1=prompt("ingresa un numero")
+let n2=prompt("ingresa un numero")
+
+let res= n1 + n2;
+console.log(res);
+
